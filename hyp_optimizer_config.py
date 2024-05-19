1228961903aa75a041cf4da581293d154d6e316b
hulkds <lequanglinhez@gmail.com>
2024-05-19 11:18:02 +0200

    update new template_task_id.

--- hyp_optimizer_config.py
+++ hyp_optimizer_config.py
@@ -4,6 +4,6 @@ from clearml.automation.optuna import OptimizerOptuna
 serach_strategy = RandomSearch # choose between RandomSearch or OptimizerOptuna
 project_name = 'aanet_depth_estimation'
 task_name = 'Optuna-hyp-optimization' 
-template_task_id = '07d47e41c23f4a5da2bd14ab68a7694e'
+template_task_id = '1d8846763f1a42e8bd7416c90a93f8d3'
 run_as_service = False
 execution_queue = 'default'
